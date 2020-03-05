b6e3caf1b4fba38e9057a40a19a1ded608ce0de3
Etienne GIROT <egirot@octo.com>
2020-03-05 10:46:01 +0100

    set up docker-file

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const Hapi = require('@hapi/hapi');
 const routes = require ('./routes')
 
 const server = Hapi.server({
-    port: 3000,
-    host: 'localhost'
+    port: process.env.PORT || 3000,
+    host:"localhost"
 });
 
 global.di = new DependenciesInjection()
